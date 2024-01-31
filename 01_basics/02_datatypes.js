5d6085761ce3ea13332f1b965b8b6a91a58ab799
Manthan Sharma <mayank9870866130@gmail.com>
2024-01-31 13:33:02 +0530

    learn about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+// Code readabilty should be high
+console.log(3 + 3);
+console.log("hello");
+
+let name = "Manthan";
+let age = 18;
+let isLoggedIn = true;
+
+// number
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof name); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
