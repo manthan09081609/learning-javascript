5c200ebcef80177f6ca5645d4a6ef0992f3d85f4
Manthan Sharma <mayank9870866130@gmail.com>
2024-02-02 23:50:38 +0530

    control flow 3

---
+++ 04_control_flow/03_switch.js
@@ -0,0 +1,65 @@
+const month = 11;
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("Febraury");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+  default:
+    console.log("Not a Valid Month");
+    break;
+}
+
+const dayName = "mon";
+switch (dayName) {
+  case "mon":
+    console.log("Monday");
+    break;
+  case "tue":
+    console.log("Tuesday");
+    break;
+  default:
+    break;
+}
+
+const day = 1;
+switch (day) {
+  case 1:
+    console.log("Monday");
+  case 2:
+    console.log("Tuesday");
+  default:
+    console.log("Not a Valid day");
+    break;
+}
